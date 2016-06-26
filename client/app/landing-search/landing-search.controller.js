d0554e96137b3d3de8b138827de1b3cd048709cd
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-06-26 13:31:05 +0200

    fix: login working now

--- client/app/landing-search/landing-search.controller.js
+++ client/app/landing-search/landing-search.controller.js
@@ -5,27 +5,6 @@
   class LandingSearchController {
 
     constructor($http, $scope) {
-
-    /*  this.mockData = [
-        { organizer: 'Pavel S.',
-          travelers:['Filip Toll', 'Bob Smith', 'Andrea Agnelli'],
-          dates : [new Date('10-6-16'), new Date('10-16-16')]
-        },
-        { organizer: 'Carlo Di Domenico',
-          travelers:['John Doe', 'Max Powers', 'Enricos De Rios'],
-          dates : [new Date('10-7-16'), new Date('10-14-16')]
-        },
-        { organizer: 'Maja May',
-          travelers:['Marius Deam', 'Francis La Porta'],
-          dates : [new Date('10-7-16'), new Date('10-12-16')]
-        },
-        {
-          organizer: 'Dann Oliver',
-          travelers: ['John Doe', 'Max Powers', 'Andrea Agnelli'],
-          dates: [new Date('10-8-16'), new Date('10-18-16')]
-        }
-      ];
-    */
       this.model = {};
       this.$http = $http;
       $scope.fromDatePickerOpen = false;
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -8,9 +8,9 @@ class NavbarController {
     this.isLoggedIn = Auth.isLoggedIn;
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
-  }
+    this.menu = ['test1', 'test2', 'test3'];
 
+  }
 }
 
-angular.module('travelWithApp')
-  .controller('NavbarController', NavbarController);
+angular.module('travelWithApp').controller('NavbarController', NavbarController);
