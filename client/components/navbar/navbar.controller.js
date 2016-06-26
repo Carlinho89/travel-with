e7f004a32b7fb7cd720696a18413e1948c7c1422
Maja <dubinska@stud.hs-heilbronn.de>
2016-06-26 15:22:57 +0200

    - added menu items - added dummy template for request form

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,16 +1,21 @@
 'use strict';
 
 class NavbarController {
-  //end-non-standard
+    //end-non-standard
 
-  //start-non-standard
-  constructor(Auth) {
-    this.isLoggedIn = Auth.isLoggedIn;
-    this.isAdmin = Auth.isAdmin;
-    this.getCurrentUser = Auth.getCurrentUser;
-    this.menu = ['test1', 'test2', 'test3'];
+    //start-non-standard
+    constructor(Auth) {
+        this.isLoggedIn = Auth.isLoggedIn;
+        this.isAdmin = Auth.isAdmin;
+        this.getCurrentUser = Auth.getCurrentUser;
+        this.menu = [
+            {
+                title: 'Requests',
+                state: 'requests'
+            },
+        ];
+    }
 
-  }
 }
 
 angular.module('travelWithApp').controller('NavbarController', NavbarController);
