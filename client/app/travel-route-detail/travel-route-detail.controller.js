ada615ed411bb082667f934a030222731bb57fd4
Maja <dubinska@stud.hs-heilbronn.de>
2016-07-02 16:16:43 +0200

    participants displayed on details page

--- client/app/travel-route-detail/travel-route-detail.controller.js
+++ client/app/travel-route-detail/travel-route-detail.controller.js
@@ -10,25 +10,35 @@
             //var routeId = '577783d0262a1b0019f41479';
             detailController.travelRoute = {};
             detailController.owner = {};
+            detailController.participants = [];
 
 
             $http.get('/api/travelroutes/' + routeId).then(
-                function (response) {
-                    // success callback
-                    detailController.travelRoute = response.data;
+                function (travelRouteResponse) {
+                    detailController.travelRoute = travelRouteResponse.data;
                     console.log(detailController.travelRoute);
-                    $http.get('/api/users/' + response.data.requestor).then(function (user) {
+                    $http.get('/api/users/' + travelRouteResponse.data.requestor).then(function (user) {
                         detailController.owner = user.data;
                         console.log(detailController.owner)
                     }, function (response) {
-                        console.log('error');
+                        console.log('FAILED TO LOAD REQUESTER FOR ROUTE ID: ' + routeId);
                         console.log(response);
                     });
+                    $http.get('/api/travelroutes/travellers/' + routeId).then(
+                        function (participantsResponse) {
+                            console.log('participants for detail page');
+                            console.log(participantsResponse);
+                            detailController.participants = participantsResponse.data;
+                        },
+                        function (response) {
+                            console.log('FAILED TO LOAD PARTICIPANTS FOR ROUTE ID: ' + routeId);
+                            console.log(response);
+                        }
+                    );
                 },
                 function (response) {
-                    console.log('error');
+                    console.log('FAILED TO LOAD TRAVEL ROUTE FOR ROUTE ID: ' + routeId);
                     console.log(response);
-                    // failure callback
                 }
             );
         }
