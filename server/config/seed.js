df818f6a4f4a113749ce6c28123c327c68aee9a4
Carlo Di Domenico <carlo.didomenico@hotmail.it>
2016-07-04 12:40:53 +0200

    removing duplicated var

--- server/config/seed.js
+++ server/config/seed.js
@@ -42,36 +42,7 @@ var test2 = new User({
     password: 'test2'
 });
 
-var testRoute = new TravelRoute({
-    name: "Bangkok's finest",
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    itinerary: [
-        {
-            name: 'Stopover in BKK',
-            startDate: new Date(2016, 7, 1),
-            endDate: new Date(2016, 7, 11),
-            place: 'Bangkok',
-            location: {
-                type: "Point",
-                coordinates: [12.123456, 13.134578]
-            }
-        },
-        {
-            name: 'Barhopping',
-            startDate: new Date(2016, 7, 11),
-            endDate: new Date(2016, 7, 14),
-            place: 'Bangkok',
-            location: {
-                type: "Point",
-                coordinates: [10.123456, 13.134578]
-            }
-        }
-    ],
-    requestor: carlo._id,
-    travellers: [admin._id]
-});
 
-testRoute.save();
 test.save();
 admin.save();
 carlo.save();
