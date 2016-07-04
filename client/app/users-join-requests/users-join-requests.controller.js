c2092e098d5c683635cdefbca23b57559ee3db2e
Maja <dubinska@stud.hs-heilbronn.de>
2016-07-04 11:52:41 +0200

    pending requestes

--- client/app/users-join-requests/users-join-requests.controller.js
+++ client/app/users-join-requests/users-join-requests.controller.js
@@ -10,12 +10,8 @@
             this.$state = $state;
             userRequestsController.requestsBy = [];
             userRequestsController.requestsFor = [];
-            userRequestsController.receiverName = '';
             userRequestsController.participants = [];
             userRequestsController.user = Auth.getCurrentUser();
-            //userRequestsController.participating = false;
-            //userRequestsController.requestSent = false;
-            //userRequestsController.isAuthor = false;
 
             var requestBy = {};
             requestBy.receiver = this.user;
@@ -61,6 +57,10 @@
                 }
             );
 
+            var requestBy = {};
+            requestBy.receiver = this.user;
+            requestBy.requestor = this.user;
+            requestBy.route = {};
             userRequestsController.$http.post('/api/join-requests/searchForUser', requestBy).then(
                 function (joinRequest) {
                     if (joinRequest.data.length != 0) {
@@ -68,6 +68,57 @@
                         console.log(joinRequest);
                         userRequestsController.requestsFor = joinRequest.data;
                     }
+                },
+                function (err) {
+                    console.log('NO REQUEST BY USER FOUND');
+                    console.log(err);
+                }
+            );
+
+            userRequestsController.$http.post('/api/join-requests/searchForUser', requestBy).then(
+                function (joinRequest) {
+                    if (joinRequest.data.length != 0) {
+                        console.log('SUCCESSFULLY LOADED EXISTING REQUESTS FOR USER:' + requestBy.receiver);
+                        console.log(joinRequest);
+                        var request = {};
+                        request.receiver = joinRequest.data[0].receiver;
+                        request.requestor = joinRequest.data[0].requestor;
+                        console.log('tutaj');
+                        console.log(request);
+                        userRequestsController.$http.get('/api/travelroutes/' + joinRequest.data[0].route).then(
+                            function (travelRouteResponse) {
+                                request.route = travelRouteResponse.data;
+                                console.log('tutaj2');
+                                console.log(request.route);
+
+                            },
+                            function (response) {
+                                console.log('FAILED TO LOAD TRAVEL ROUTE FOR ROUTE');
+                                console.log(response);
+                            }
+                        );
+
+                        userRequestsController.$http.get('/api/users/' + request.receiver).then(function (user) {
+                            console.log('tutaj3');
+                            request.receiverName = user.data.name;
+                        }, function (response) {
+                            console.log('FAILED TO LOAD REQUESTER FOR ROUTE ID: ' + detailController.routeId);
+                            console.log(response);
+                        });
+
+                        userRequestsController.$http.get('/api/users/' + request.requestor).then(function (user) {
+                            console.log('tutaj4');
+                            request.requestorName = user.data.name;
+                        }, function (response) {
+                            console.log('FAILED TO LOAD REQUESTER FOR ROUTE ID: ' + detailController.routeId);
+                            console.log(response);
+                        });
+                        console.log('tutaj5');
+                        console.log(request);
+                        userRequestsController.requestsFor.push(request);
+                        console.log(userRequestsController.requestsFor);
+                    }
+
                 },
                 function (err) {
                     console.log('NO REQUEST BY USER FOUND');
